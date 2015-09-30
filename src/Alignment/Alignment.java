bb75d681e22baa217a7e92d378727bd804175205
Todd Beckman <toddbeckman1@gmail.com>
2015-09-30 10:46:10 -0600

    fleshed out more of the code

--- src/Alignment/Alignment.java
+++ src/Alignment/Alignment.java
@@ -1,4 +1,7 @@
 package Alignment;
+
+import Logger.Logger;
+
 public class Alignment {
     //  Score of a match
     public static final int MATCH_SCORE = 1;
@@ -24,6 +27,7 @@ public class Alignment {
 //                  + "strings of different lengths!");
 //      }
         int[][] table = new int[w][h];
+        path = new int[w][h];
         //  Call the recursive, dynamic programming method
         //  Use the bottom right corner
         score(table, str1, str2, w - 1, h - 1);
@@ -33,37 +37,55 @@ public class Alignment {
     //  Recursive dynamic programming method to populate table
     private static int score (int[][] table, String str1, String str2, int i1, int i2) {
         int[] previous = new int[3];
+        
         //  Base case: Top edge of the table
         if (i1 == 0) {
-            previous[0] = BORDER_SCORE;
+            previous[U] = BORDER_SCORE;
         }
         else {
-            previous[0] = score (table, str1, str2, i1 - 1, i2);
+            //  Get the score from above
+            previous[U] = score (table, str1, str2, i1 - 1, i2);
         }
+        
         //  Base case: Left edge of the table
         if (i1 == 0) {
-            previous[1] = BORDER_SCORE;
+            previous[L] = BORDER_SCORE;
         }
         else {
-            previous[1] = score (table, str1, str2, i1, i2 - 1);
+            //  Get the score from the left
+            previous[L] = score (table, str1, str2, i1, i2 - 1);
         }
+        
         //  Base case: Either edge of the table
         if (i1 == 0 || i2 == 0) {
-            previous[2] = BORDER_SCORE;
+            previous[UL] = BORDER_SCORE;
         }
         else {
-            previous[2] = score (table, str1, str2, i1 - 1, i2 - 1);
+            //  Get the score from the upper left
+            previous[UL] = score (table, str1, str2, i1 - 1, i2 - 1);
         }
-        //  TODO: populate rest of scoring mechanics
         
-        //  dummy return
-        return 0;
+        //  Find the top-scoring cell
+        int max = max_index(previous);
+        
+        //  Update the path
+        path[i1][i2] = max;
+        
+        //  Return the highest score from this run
+        return previous[max];
     }
+    
+    //  Used internally to identify direction
+    private static final int UL = 0;    //  favor this in case of tie
+    private static final int U = 1;
+    private static final int L = 2;
+    
     //  Used internally to construct the Dynamic Programming path
-    private static String path = "";
+    private static int[][] path;
+    
     //  Used internally to find the index of the max of a list
     private static int max_index (int[] arr) {
-        int max = 0;
+        int max = 0;    //  favors UL in case of tie
         for (int i = 1; i < arr.length; i++) {
             if (arr[max] < arr[i]) {
                 max = i;
@@ -72,6 +94,6 @@ public class Alignment {
         return max;
     }
     public static void main(String[] args) {
-        //  TODO
+        
     }
 }
--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -1,6 +1,7 @@
 package Logger;
 
 public class Logger {
+    //  The goal is to print to file; that is not important yet
     public static void log(String message) {
         System.out.println(message);
     }
