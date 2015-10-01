3157a0e4ae9cb3f3be8b75c6d4f88ce6b3d9bade
Todd Beckman <toddbeckman1@gmail.com>
2015-09-30 23:50:22 -0600

    separated main from alignment

--- src/Alignment/Alignment.java
+++ src/Alignment/Alignment.java
@@ -1,9 +1,5 @@
 package Alignment;
 
-import java.util.Scanner;
-
-import Logger.Logger;
-
 public class Alignment {
     //  Score of a match
     public static int MATCH_SCORE = 1;
@@ -133,10 +129,10 @@ public class Alignment {
     private static final int L = 2;
     
     //  Used internally to construct the Dynamic Programming scores
-    private static int[][] scores;
+    static int[][] scores;
     
     //  Used internally to construct the Dynamic Programming path
-    private static int[][] path;
+    static int[][] path;
     
     //  Used internally to find the index of the max of a list
     private static int max_index (int[] arr) {
@@ -147,42 +143,5 @@ public class Alignment {
             }
         }
         return max;
-    }
-    private static String genString(int length, char[] alphabet) {
-        char[] str = new char[length];
-        for (int i = 0; i < length; i++){
-            str[i] = alphabet[(int)(Math.random() * alphabet.length)];
-        }
-        return String.valueOf(str);
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What score should a match yield?           ");
-        MATCH_SCORE = scanner.nextInt();
-        System.out.print("What score should a mismatch yield?        ");
-        MISMATCH_SCORE = scanner.nextInt();
-        System.out.print("What score should an indel yield?          ");
-        INDEL_SCORE = scanner.nextInt();
-        System.out.print("How many times should the program run?     ");
-        int ITERATIONS = scanner.nextInt();
-        System.out.print("What should the lengths of the strings be? ");
-        int LENGTH = scanner.nextInt();
-        scanner.close();
-        System.out.println("Working...");
-        String logfile = "logs_" + ITERATIONS + "x" + LENGTH +".txt";
-        
-        char[] alphabet = {'A', 'C', 'G', 'T'};
-        Logger.open(logfile);
-        for (int i = 0; i < ITERATIONS; i++) {
-            String str1 = genString(LENGTH, alphabet);
-            String str2 = genString(LENGTH, alphabet);
-            Logger.log("1: " + str1);
-            Logger.log("2: " + str2);
-            Logger.log("R: " + globalAlignment(str1, str2));
-            Logger.log("Score: " + scores[LENGTH - 1][LENGTH - 1]);
-            Logger.log();
-        }
-        Logger.close();
-        System.out.println("Finished! You can find the results at " + logfile + ".");
     }
 }
---
+++ src/Alignment/Main.java
@@ -0,0 +1,45 @@
+package Alignment;
+import java.util.Scanner;
+
+import Logger.Logger;
+
+public class Main {
+
+    private static String genString(int length, char[] alphabet) {
+        char[] str = new char[length];
+        for (int i = 0; i < length; i++){
+            str[i] = alphabet[(int)(Math.random() * alphabet.length)];
+        }
+        return String.valueOf(str);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What score should a match yield?           ");
+        Alignment.MATCH_SCORE = scanner.nextInt();
+        System.out.print("What score should a mismatch yield?        ");
+        Alignment.MISMATCH_SCORE = scanner.nextInt();
+        System.out.print("What score should an indel yield?          ");
+        Alignment.INDEL_SCORE = scanner.nextInt();
+        System.out.print("How many times should the program run?     ");
+        int ITERATIONS = scanner.nextInt();
+        System.out.print("What should the lengths of the strings be? ");
+        int LENGTH = scanner.nextInt();
+        scanner.close();
+        System.out.println("Working...");
+        String logfile = "logs_" + ITERATIONS + "x" + LENGTH +".txt";
+        
+        char[] alphabet = {'A', 'C', 'G', 'T'};
+        Logger.open(logfile);
+        for (int i = 0; i < ITERATIONS; i++) {
+            String str1 = genString(LENGTH, alphabet);
+            String str2 = genString(LENGTH, alphabet);
+            Logger.log("1: " + str1);
+            Logger.log("2: " + str2);
+            Logger.log("R: " + Alignment.globalAlignment(str1, str2));
+            Logger.log("Score: " + Alignment.scores[LENGTH - 1][LENGTH - 1]);
+            Logger.log();
+        }
+        Logger.close();
+        System.out.println("Finished! You can find the results at " + logfile + ".");
+    }
+}
